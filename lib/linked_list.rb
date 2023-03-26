ad7ad0b3a6d3e075cbd1c907864ac2bc8ebe52da
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-03-26 12:47:55 -0500

    Create class LinkedList and define method add_node

---
+++ lib/linked_list.rb
@@ -0,0 +1,18 @@
+class LinkedList
+  attr_reader :head, :tail
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+  def add_node(data)
+    new_node = Node.new(data)
+
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      @tail.next_node = new_node
+      @tail = new_node
+    end
+  end
+end
