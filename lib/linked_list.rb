213d17c326a8ceb9c25f514a0b071f6015a5d610
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-03-26 22:35:46 -0500

    Add count method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,6 +4,7 @@ class LinkedList
     @head = nil
     @tail = nil
   end
+
   def add_node(data)
     new_node = Node.new(data)
 
@@ -15,7 +16,44 @@ class LinkedList
       @tail = new_node
     end
   end
+
   def append(data)
-    @head = Node.new(data)
+    if @head == nil
+      @head = Node.new(data)
+    else
+      current_node = @head
+      # new_node = Node.new(data)
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+      end
+      current_node.next_node = Node.new(data)
+    end
+  end
+
+  def count
+    if self == nil
+      count = 0
+    else
+      count = 1
+      current_node = @head
+      while current_node.next_node != nil
+        current_node = current_node.next_node
+        count +=1
+      end
+      count
+    end
+  end
+
+  def to_string
+    current_node = head
+    jbeat = []
+    until current_node == nil
+      jbeat << current_node.data
+      current_node = current_node.next_node
+    end
+      jbeat.join(" ")
   end
 end
+
+
+
