15cc92e33c82d902589a901144f5c339f83d260b
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-03-28 16:50:04 -0500

    Create append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,8 +1,7 @@
 class LinkedList
-  attr_reader :head, :tail
+  attr_reader :head
   def initialize
     @head = nil
-    @tail = nil
   end
 
   def add_node(data)
@@ -59,6 +58,59 @@ class LinkedList
     end
       jbeat.join(" ")
   end
+
+  # def index_at(value_index)
+  #   current_node = head
+  #   (index - 1).times do
+  #     if current_node.nil?
+  #       current_node = current_node.next_node
+  #     end
+  #   end
+  # end
+
+  def insert(index, data)
+    current_node = @head 
+    (index - 1).times do
+    current_node = current_node.next_node
+    # require "pry"; binding.pry
+    end
+    new_node = Node.new(data)
+    new_node.next_node = current_node.next_node
+    current_node.next_node = new_node
+  end
+
+  def find(index, string_to_return)
+    current_node = @head
+    beats = []
+    index.times do
+      current_node = current_node.next_node
+    end
+    while current_node != nil && beats.length < string_to_return
+      beats << current_node.data
+      current_node = current_node.next_node
+    end
+    beats.join(" ")
+  end
+
+  def includes?(data)
+    current_node = @head
+    until current_node.data == data || current_node.next_node == nil
+      current_node = current_node.next_node
+    end
+    current_node.data == data
+  end
+
+  def pop
+    current_node = @head
+    until current_node.next_node.next_node == nil
+      current_node = current_node.next_node
+    end
+    tail = current_node.next_node
+    current_node.next_node = nil
+    tail.data
+  end
 end
+
+
 
 
